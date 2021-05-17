e7d81accfd8c878798f2c1724487f6cd77e8dff8
Kevin Gasperich <kgasperich@gmail.com>
2021-05-17 19:49:16 +0000

    3d -> 1d

--- openmp/target_map/arrays_of_structs/mwe-no_pointer/small/small.F90
+++ openmp/target_map/arrays_of_structs/mwe-no_pointer/small/small.F90
@@ -1,11 +1,9 @@
 module prim
   implicit none
   integer :: nx = 10
-  integer :: ny = 10
-  integer :: nz = 10
 
   type prim_type
-    double precision, allocatable, dimension(:,:,:) :: v1
+    double precision, allocatable, dimension(:) :: v1
   contains 
     procedure :: setup => setup_prim
   end type prim_type
@@ -14,7 +12,7 @@ contains
 
   subroutine setup_prim(prim_data_i)
   class(prim_type), intent(OUT) :: prim_data_i
-    allocate(prim_data_i%v1(nx,ny,nz))
+    allocate(prim_data_i%v1(nx))
   end subroutine setup_prim
 
 end module prim
@@ -22,7 +20,7 @@ end module prim
 
 module objects
   use prim
-  type(prim_type)  ::    prim_data(1:10)
+  type(prim_type)  ::    prim_data(1:1)
 end module objects
 
 
@@ -36,11 +34,11 @@ program testmap
   call prim_data(id)%setup()
 
   ! Set data on host
-  prim_data(id)%v1(1,1,1) = 9.99D0
+  prim_data(id)%v1(1) = 9.99D0
   
   ! set data on device 
   !$omp target
-  prim_data(id)%v1(1,1,1) = 1.0D0
+  prim_data(id)%v1(1) = 1.0D0
   !$omp end target
 
 end program testmap
